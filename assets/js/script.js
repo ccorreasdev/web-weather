d16ae963c42f0d906b9598e2fa19bdb197da6dd2
ccorreas <ccorreas@MacBook-Air-de-Cristian.local>
2024-05-24 15:49:48 +0200

    Added CSS elements

--- assets/js/script.js
+++ assets/js/script.js
@@ -44,16 +44,22 @@ const cardCity = document.querySelector("#card-city");
 const cardTemp = document.querySelector("#card-temp");
 const cardDesc = document.querySelector("#card-desc");
 const cardIcon = document.querySelector("#card-icon");
+const cardItem1 = document.querySelector("#item1");
+const cardItem2 = document.querySelector("#item2");
+const cardItem3 = document.querySelector("#item3");
+const cardItem4 = document.querySelector("#item4");
+const getWeatherBtn = document.querySelector("#get-weather");
+const cityInput = document.querySelector("#city-input");
 const API_KEY = "cffcff2eaa4b49a694a7c42b57d1324b";
 const API_URL = " https://api.weatherbit.io/v2.0/current?";
-const API_CITY = "Novelda"
+let API_CITY = "Novelda"
 const API_LANGUAGE = "es"
-const API_ACTUAL_WEATHER = `${API_URL}city=${API_CITY}&key=${API_KEY}&lang=${API_LANGUAGE}`;
+
 let actualWeather = undefined;
 
 const getActualWeather = async () => {
     try {
-
+        const API_ACTUAL_WEATHER = `${API_URL}city=${API_CITY}&key=${API_KEY}&lang=${API_LANGUAGE}`;
         const response = await fetch(API_ACTUAL_WEATHER);
 
         if (!response.ok) {
@@ -62,22 +68,24 @@ const getActualWeather = async () => {
 
         actualWeather = await response.json();
 
+
     } catch (error) {
 
         console.error('There has been a problem with your fetch operation:', error);
     }
 }
 
-
-const init = async () => {
-    await getActualWeather();
+const setActualWeather = () => {
     if (actualWeather) {
         console.log(actualWeather);
         console.log(`Temperature in ${API_CITY}: ${actualWeather.data[0].temp}°C`);
         cardCity.innerHTML = actualWeather.data[0].city_name;
         cardTemp.innerHTML = `${actualWeather.data[0].temp}°C`;
         cardDesc.innerHTML = actualWeather.data[0].weather.description;
-
+        cardItem1.innerHTML = actualWeather.data[0].precip + "% precipitaciones";
+        cardItem2.innerHTML = actualWeather.data[0].clouds + "% nubosidad";
+        cardItem3.innerHTML = actualWeather.data[0].pres + " mbar";
+        cardItem4.innerHTML = actualWeather.data[0].uv + " uv";
         const weatherCode = actualWeather.data[0].weather.code;
 
         if (weatherCode == 800) {
@@ -89,12 +97,27 @@ const init = async () => {
         } else if (weatherCode >= 600 && weatherCode <= 623) {
             cardIcon.src = "./assets/img/weather/Snowy.png"
         } else if (weatherCode >= 801 && weatherCode <= 804) {
-            cardIcon.src = "./assets/img/weather/PartyCloudly.png"
+            cardIcon.src = "./assets/img/weather/PartlyCloudy.png"
         } else {
-            cardIcon.src = "./assets/img/weather/PartyCloudly.png"
+            cardIcon.src = "./assets/img/weather/PartlyCloudy.png"
         }
     }
+}
+
+const init = async () => {
+    // await getActualWeather();
+    //setActualWeather();
 
 }
 
 init();
+
+getWeatherBtn.addEventListener("click", async (e) => {
+
+    if (cityInput.value) {
+        API_CITY = await cityInput.value;
+        await getActualWeather();
+        setActualWeather();
+    }
+
+})
